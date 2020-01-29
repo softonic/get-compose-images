b624ab05483cfc2e3282005cf3b145ac2988e8f2
Basilio Vera <basilio.vera@softonic.com>
2020-01-29 17:27:27 +0100

    Filter services without image
    Signed-off-by: Basilio Vera <basilio.vera@softonic.com>

--- get-compose-images.sh
+++ get-compose-images.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker-compose config |\
-  yq -crM '.services | with_entries(select(.value | has("build"))) | to_entries | map(. | {service: .key, image: .value.image, dockerfile: .value.build.dockerfile})'
+  yq -crM '.services | with_entries(select(.value | has("build"))) | to_entries | map(. | {service: .key, image: .value.image, dockerfile: .value.build.dockerfile}) | map( select(.image != null) | with_entries( select( .value != 0 )) )'
